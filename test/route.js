9d3fe0d76816cc4927c28cc4ce33f8a7a3d9b2dd
Ingrid <ingridcalzada@MacBook-Pro-de-Ingrid-2.local>
2024-05-28 18:08:02 -0500

    m commit ingrid

--- test/route.js
+++ test/route.js
@@ -1,59 +1,52 @@
-var request = require('supertest');  
+var request = require("supertest");
 // hay que arrancar el servidor antes. Más adelante probaremos esto http://51elliot.blogspot.com.es/2013/08/testing-expressjs-rest-api-with-mocha.html
 
-var server=require(__dirname + '/../web.js');
+var server = require(__dirname + "/../web.js");
 var port = Number(process.env.PORT || 5000);
-request=request("http://localhost:"+port);
+request = request("http://localhost:" + port);
 
-describe('Web', function(){
+describe("Web", function () {
+  it("Debería devolver pero poco  la raíz", function () {
+    request
+      .get("/")
+      .expect(200)
+      .end(function (err, res) {
+        if (err) {
+          throw err;
+        }
+      });
+  });
 
-	     it('Debería devolver la raíz', function(){
-		    request.get("/")
-			.expect(200)
-			.end(function(err,res) {
-				 if (err) {
-				     throw err ;
-				 }
-			     });
-		});
+  it("Debería devolver uso_basico", function () {
+    request
+      .get("/curso/texto/uso_basico")
+      .expect(200)
+      .end(function (err, res) {
+        if (err) {
+          throw err;
+        }
+      });
+  });
 
-	     
+  it("Debería devolver una imagen", function () {
+    request
+      .get("/img/wingit1.png")
+      .expect(200)
+      .end(function (err, res) {
+        if (err) {
+          throw err;
+        }
+      });
+  });
 
-	     it('Debería devolver uso_basico', function(){
-		    request.get("/curso/texto/uso_basico")
-			.expect(200)
-			.end(function(err,res) {
-				 if (err) {
-				     throw err ;
-				 }
-			     });
-		});
-
-	     
-	     it('Debería devolver una imagen', function(){
-		    request.get("/img/wingit1.png")
-			.expect(200)
-			.end(function(err,res) {
-				 if (err) {
-				     throw err ;
-				 }
-			     });
-		});
-
-	     
-	     it('Debería devolver una imagen ', function(){
-		    request.get("/curso/texto/img/wingit2.png")
-			.expect(200)
-			.end(function(err,res) {
-				 if (err) {
-				     throw err ;
-				 }
-			     });
-		});
-
-
-
-
-
+  it("Debería devolver una imagen ", function () {
+    request
+      .get("/curso/texto/img/wingit2.png")
+      .expect(200)
+      .end(function (err, res) {
+        if (err) {
+          throw err;
+        }
+      });
+  });
 });
-
